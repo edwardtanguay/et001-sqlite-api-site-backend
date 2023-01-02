63f5b90c377082adef5794fbb1e7f64a01486fa1
Edward Tanguay (windows) <edward@tanguay.info>
2023-01-02 02:00:22 +0100

    test message

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,20 @@
 import express from 'express';
 import cors from 'cors';
 import * as config from './config.js';
+import * as model from './model.js';
 
 const app = express();
 app.use(cors());
 
 app.get('/', (req: express.Request, res: express.Response) => {
-	res.send('test');
+	res.send(model.getApiInstructions());
+});
+
+app.get('/test', (req: express.Request, res: express.Response) => {
+	const datetime = new Date();
+	res.json({
+		message: `API test accessed at ${datetime.toISOString()}`
+	});
 });
 
 
